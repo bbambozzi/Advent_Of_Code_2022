e81ea66c4bf3158d5d4235369e8f859ea7f3b2e6
Bautista Bambozzi <bautistabambozzi@gmail.com>
2022-12-05 18:48:32 -0300

    Finished 4!

---
+++ 4/p1.py
@@ -0,0 +1,10 @@
+with open('input.txt', 'r') as f:
+    count = 0 
+    for line in f.readlines():
+        l , r = line.strip().split(',')[0] , line.strip().split(',')[1]
+        l1, l2 = int(l.split('-')[0]) , int(l.split('-')[1])
+        r1, r2 = int(r.split('-')[0]) , int(r.split('-')[1])
+        if (l1>=r1 and l2 <= r2) or (l1 <=r1 and l2>= r2):
+            count += 1
+    print(count)
+            
